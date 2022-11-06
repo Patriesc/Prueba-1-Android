6266691c8baa091da7160044a518c916116944e8
Patricia Sanz Escurin <patricia.sanz.escurin@gmail.com>
2022-11-06 15:23:09 +0100

    Definido el modelo de datos

---
+++ app/src/main/java/com/example/prueba1/Datos.java
@@ -0,0 +1,38 @@
+package com.example.prueba1;
+
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.io.Serializable;
+
+public class Datos implements Serializable {
+    @Entity(tableName = "table_name")
+    public class MainData implements Serializable {
+
+        // Definimos el modelo de datos
+        @PrimaryKey(autoGenerate = true)
+        private int ID;
+
+        @ColumnInfo(name = "texto")
+        private String texto;
+
+
+        public int getID() {
+            return ID;
+        }
+
+        public void setID(int ID) {
+            this.ID = ID;
+        }
+
+        public String getTexto() {
+            return texto;
+        }
+
+        public void setTexto(String texto) {
+            this.texto = texto;
+        }
+    }
+}
