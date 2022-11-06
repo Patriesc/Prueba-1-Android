460b8ebd466357416013a2cf4f7ea24f54bf0f17
Patricia Sanz Escurin <patricia.sanz.escurin@gmail.com>
2022-11-06 15:15:42 +0100

    Layout creado

--- app/src/main/java/com/example/prueba1/MainActivity.java
+++ app/src/main/java/com/example/prueba1/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.prueba1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    LinearLayoutManager linearLayoutManager;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Iniciamos Layout
+        linearLayoutManager = new LinearLayoutManager(this);
     }
 }
