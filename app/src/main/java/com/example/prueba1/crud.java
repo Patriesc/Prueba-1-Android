e2db53944c934564cdf08ee29007d1eba7ef2a40
Patricia Sanz Escurin <patricia.sanz.escurin@gmail.com>
2022-11-06 16:34:54 +0100

    Interfaz para acceso a datos completada

---
+++ app/src/main/java/com/example/prueba1/crud.java
@@ -0,0 +1,30 @@
+package com.example.prueba1;
+
+import static androidx.room.OnConflictStrategy.REPLACE;
+
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+public interface crud {
+    @Insert(onConflict = REPLACE)
+    void insert(Datos datosMain);
+
+    // Borrar
+    @Delete
+    void delete(Datos datosMain);
+
+    // Borrar todo
+    @Delete
+    void reset(List<Datos> datosMain);
+
+    //Actualizar
+    @Query("UPDATE Table_name SET texto =:sText WHERE ID = :sID")
+    void upate(int sID,String sText);
+
+    // Devolver todos los datos
+    @Query("SELECT * FROM table_name")
+    List<Datos> getAll();
+}
